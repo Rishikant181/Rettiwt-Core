d38b0435fcf0ba9bb8163e09170097cbed98b5d6
Rishikant Sahu <rishikantsahu181@gmail.com>
2023-05-31 20:03:48 +0530

    Updated project structure

--- src/index.ts
+++ src/index.ts
@@ -9,21 +9,21 @@ export { Url } from './Url';
 export * from './types/request/params/Query';
 
 // Exporting raw types related to tweets
-export { Root as ITweetLikesResponse } from './types/tweet/Favouriters';
-export { Root as ITweetRetweetsResponse } from './types/tweet/Retweeters';
-export { Root as ITweetDetailsResponse } from './types/tweet/Tweet';
-export { Root as ITweetsResponse } from './types/tweet/Tweets';
+export { Root as ITweetLikesResponse } from './types/response/tweet/Favouriters';
+export { Root as ITweetRetweetsResponse } from './types/response/tweet/Retweeters';
+export { Root as ITweetDetailsResponse } from './types/response/tweet/Tweet';
+export { Root as ITweetsResponse } from './types/response/tweet/Tweets';
 
 // Exporting raw types related to users
-export { Root as IUserFollowersResponse } from './types/user/Followers';
-export { Root as IUserFollowingResponse } from './types/user/Following';
-export { Root as IUserLikesResponse } from './types/user/Likes';
-export { Root as IUserTweetsResponse } from './types/user/Tweets';
-export { Root as IUserDetailsResponse } from './types/user/User';
+export { Root as IUserFollowersResponse } from './types/response/user/Followers';
+export { Root as IUserFollowingResponse } from './types/response/user/Following';
+export { Root as IUserLikesResponse } from './types/response/user/Likes';
+export { Root as IUserTweetsResponse } from './types/response/user/Tweets';
+export { Root as IUserDetailsResponse } from './types/response/user/User';
 
 // Exporting base raw types
-export { Result as IRawTweet } from './types/tweet/Tweet';
-export { Result as IRawUser } from './types/user/User';
+export { Result as IRawTweet } from './types/response/tweet/Tweet';
+export { Result as IRawUser } from './types/response/user/User';
 
 // Exporting payload types
 export { IArgs } from './types/request/payloads/Args';
