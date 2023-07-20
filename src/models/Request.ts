5ca798e1f9039e1ae1692ec6d4f23a75cb5368a1
Rishikant Sahu <rishikantsahu181@gmail.com>
2023-07-20 17:27:17 +0530

    Updated Request type and model

--- src/models/Request.ts
+++ src/models/Request.ts
@@ -1,6 +1,8 @@
 // ENUMS
 import { ERequestType } from "../enums/RequestType";
 import { EResourceType } from "../enums/Resources";
+
+// MODELS
 import { Url } from "./Url";
 import { Args } from "./payloads/Args";
 
@@ -8,18 +10,20 @@ export class Request<PayloadType> {
     /** The full url of the request. */
     url: string;
 
-    /** The resource targeted by the request. */
-    resource: EResourceType;
-
     /** The type of 'this' request. */
     type: ERequestType;
 
     /** The payload to be sent in the request. */
     payload?: PayloadType;
 
-    constructor(resourceType: EResourceType, args?: Args, payload?: PayloadType) {
-        this.resource = resourceType;
-        this.url = new Url(resourceType, args);
-        this.payload = payload;
+    /**
+     * Generates an HTTP request configuration for the requested resource on Twitter.
+     * 
+     * @param resourceType - The type of resource requested.
+     * @param args - Additional URL arguments.
+     */
+    constructor(resourceType: EResourceType, args: Args) {
+        this.url = new Url(resourceType, args ?? {}).toString();
+        this.type = ERequestType.GET;
     }
 }
--- src/types/Request.ts
+++ src/types/Request.ts
@@ -1,13 +1,9 @@
 // ENUMS
 import { ERequestType } from "../enums/RequestType";
-import { EResourceType } from "../enums/Resources";
 
 export interface IRequest<PayloadType> {
     /** The full url of the request. */
     url: string;
-
-    /** The resource targeted by the request. */
-    resource: EResourceType;
 
     /** The type of 'this' request. */
     type: ERequestType;
