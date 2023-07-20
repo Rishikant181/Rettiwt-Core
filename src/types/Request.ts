b8244dd0a66b346ac41919851128a2e4c3ba86dc
Rishikant Sahu <rishikantsahu181@gmail.com>
2023-07-20 07:58:45 +0530

    Added Request interface

---
+++ src/types/Request.ts
@@ -0,0 +1,17 @@
+// TYPES
+import { ERequestType } from "../enums/RequestType";
+import { EResourceType } from "../enums/Resources";
+
+export interface IRequest<PayloadType> {
+    /** The full url of the request. */
+    url: string;
+
+    /** The resource targeted by the request. */
+    resource: EResourceType;
+
+    /** The type of 'this' request. */
+    type: ERequestType;
+
+    /** The payload to be sent (applicable for {@link ERequestType.POST}) request. */
+    payload?: PayloadType;
+}
