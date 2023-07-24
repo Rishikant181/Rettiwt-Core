89b1f1011d6756dcbe61c151cad018d7ba8eb81a
Rishikant Sahu <rishikantsahu181@gmail.com>
2023-07-24 16:12:38 +0530

    Fixed a bug where request type was incorrectly set as 'GET' for retweet and favorite actions

--- src/types/response/tweet/Retweet.ts
+++ src/types/response/tweet/Retweet.ts
@@ -6,26 +6,26 @@
  * @public
  */
 export interface Root {
-    data: Data
+	data: Data;
 }
 
 export interface Data {
-    create_retweet: CreateRetweet
+	create_retweet: CreateRetweet;
 }
 
 export interface CreateRetweet {
-    retweet_results: RetweetResults
+	retweet_results: RetweetResults;
 }
 
 export interface RetweetResults {
-    result: Result
+	result: Result;
 }
 
 export interface Result {
-    rest_id: string
-    legacy: Legacy
+	rest_id: string;
+	legacy: Legacy;
 }
 
 export interface Legacy {
-    full_text: string
+	full_text: string;
 }
