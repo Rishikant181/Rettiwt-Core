371687c77de19a87ccc526f974f5e105771869f7
omarcinkonis <osmundas@bitdegree.org>
2023-10-09 15:22:57 +0300

    Added reply fetching

--- src/models/payloads/Args.ts
+++ src/models/payloads/Args.ts
@@ -48,6 +48,7 @@ export class Args implements IArgs {
 			EResourceType.USER_FOLLOWING,
 			EResourceType.USER_LIKES,
 			EResourceType.USER_TWEETS,
+			EResourceType.USER_TWEETS_AND_REPLIES,
 		],
 	})
 	@IsNumberString(undefined, {
@@ -64,6 +65,7 @@ export class Args implements IArgs {
 			EResourceType.USER_FOLLOWING,
 			EResourceType.USER_LIKES,
 			EResourceType.USER_TWEETS,
+			EResourceType.USER_TWEETS_AND_REPLIES,
 		],
 	})
 	public id?: string;
@@ -86,6 +88,7 @@ export class Args implements IArgs {
 			EResourceType.USER_FOLLOWERS,
 			EResourceType.USER_FOLLOWING,
 			EResourceType.USER_LIKES,
+			EResourceType.USER_TWEETS_AND_REPLIES,
 		],
 	})
 	@Max(20, { groups: [EResourceType.TWEET_SEARCH, EResourceType.USER_TWEETS] })
