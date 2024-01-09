8f969180ccf831045172ce6254e0fd8cd3409f68
Rishikant Sahu <rishikantsahu181@gmail.com>
2024-01-09 16:58:24 +0530

    Added endpoint for getting live video stream

--- src/enums/Resources.ts
+++ src/enums/Resources.ts
@@ -14,15 +14,20 @@ export enum ESubdomains {
  * @public
  */
 export enum EResourceType {
+	// TWEET
 	CREATE_RETWEET = '/i/api/graphql/ojPdsZsimiJrUGLR1sjUtA/CreateRetweet',
 	CREATE_TWEET = '/i/api/graphql/tTsjMKyhajZvK4q76mpIBg/CreateTweet',
 	FAVORITE_TWEET = '/i/api/graphql/lI07N6Otwv1PhnEgXILM7A/FavoriteTweet',
-	LIST_DETAILS = '/i/api/graphql/gO1_eYPohKYHwCG2m-1ZnQ/ListByRestId',
-	LIST_TWEETS = '/i/api/graphql/naCjgapXCSCsbZ7qnnItQA/ListLatestTweetsTimeline',
 	TWEET_SEARCH = '/i/api/graphql/nK1dw4oV3k4w5TdtcAdSww/SearchTimeline',
 	TWEET_DETAILS = '/i/api/graphql/0hWvDhmW8YQ-S_ib3azIrw/TweetResultByRestId',
 	TWEET_FAVORITERS = '/i/api/graphql/9XKD3EWWC2BKpIFyDj4KKQ/Favoriters',
 	TWEET_RETWEETERS = '/i/api/graphql/v5h-KLmyl-wqZ8i-a_q73w/Retweeters',
+
+	// LIST
+	LIST_DETAILS = '/i/api/graphql/gO1_eYPohKYHwCG2m-1ZnQ/ListByRestId',
+	LIST_TWEETS = '/i/api/graphql/naCjgapXCSCsbZ7qnnItQA/ListLatestTweetsTimeline',
+
+	// USER
 	USER_DETAILS = '/i/api/graphql/oUZZZ8Oddwxs8Cd3iW3UEA/UserByScreenName',
 	USER_DETAILS_BY_ID = '/i/api/graphql/i_0UQ54YrCyqLUvgGzXygA/UserByRestId',
 	USER_FOLLOWING = '/i/api/graphql/V8zHw0SZijWORSsb-FNrng/Following',
@@ -30,6 +35,11 @@ export enum EResourceType {
 	USER_LIKES = '/i/api/graphql/kgZtsNyE46T3JaEf2nF9vw/Likes',
 	USER_TWEETS = '/i/api/graphql/H8OOoI-5ZE4NxgRr8lfyWg/UserTweets',
 	USER_TWEETS_AND_REPLIES = '/i/api/graphql/GJuqLMoVxFZOvy8Oqec_9Q/UserTweetsAndReplies',
+
+	// MEDIA
 	MEDIA_UPLOAD = '/i/media/upload.json',
+	LIVE_VIDEO_STREAM = '/i/api/1.1/live_video_stream/status',
+
+	// SPACE
 	AUDIO_SPACE_DETAILS_BY_ID = '/i/api/graphql/s2tz6GAie-O1tdZx873PLA/AudioSpaceById',
 }
