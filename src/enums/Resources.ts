43206a1f52a2960c179ac08592acd160ad824401
Rishikant Sahu <rishikantsahu181@gmail.com>
2024-03-01 17:07:28 +0530

    Updated docs

--- src/enums/Resources.ts
+++ src/enums/Resources.ts
@@ -14,6 +14,17 @@ export enum ESubdomains {
  * @public
  */
 export enum EResourceType {
+	// LIST
+	LIST_DETAILS = '/i/api/graphql/gO1_eYPohKYHwCG2m-1ZnQ/ListByRestId',
+	LIST_TWEETS = '/i/api/graphql/naCjgapXCSCsbZ7qnnItQA/ListLatestTweetsTimeline',
+
+	// MEDIA
+	MEDIA_UPLOAD = '/i/media/upload.json',
+	VIDEO_STREAM = '/i/api/1.1/live_video_stream/status',
+
+	// SPACE
+	SPACE_DETAILS_BY_ID = '/i/api/graphql/s2tz6GAie-O1tdZx873PLA/AudioSpaceById',
+
 	// TWEET
 	CREATE_RETWEET = '/i/api/graphql/ojPdsZsimiJrUGLR1sjUtA/CreateRetweet',
 	CREATE_TWEET = '/i/api/graphql/tTsjMKyhajZvK4q76mpIBg/CreateTweet',
@@ -22,10 +33,6 @@ export enum EResourceType {
 	TWEET_DETAILS = '/i/api/graphql/0hWvDhmW8YQ-S_ib3azIrw/TweetResultByRestId',
 	TWEET_FAVORITERS = '/i/api/graphql/9XKD3EWWC2BKpIFyDj4KKQ/Favoriters',
 	TWEET_RETWEETERS = '/i/api/graphql/v5h-KLmyl-wqZ8i-a_q73w/Retweeters',
-
-	// LIST
-	LIST_DETAILS = '/i/api/graphql/gO1_eYPohKYHwCG2m-1ZnQ/ListByRestId',
-	LIST_TWEETS = '/i/api/graphql/naCjgapXCSCsbZ7qnnItQA/ListLatestTweetsTimeline',
 
 	// USER
 	USER_DETAILS = '/i/api/graphql/oUZZZ8Oddwxs8Cd3iW3UEA/UserByScreenName',
@@ -38,11 +45,4 @@ export enum EResourceType {
 	USER_SUBSCRIPTIONS = '/i/api/graphql/UWlxAhUnBNK0BYmeqNPqAw/UserCreatorSubscriptions',
 	USER_TWEETS = '/i/api/graphql/H8OOoI-5ZE4NxgRr8lfyWg/UserTweets',
 	USER_TWEETS_AND_REPLIES = '/i/api/graphql/GJuqLMoVxFZOvy8Oqec_9Q/UserTweetsAndReplies',
-
-	// MEDIA
-	MEDIA_UPLOAD = '/i/media/upload.json',
-	VIDEO_STREAM = '/i/api/1.1/live_video_stream/status',
-
-	// SPACE
-	SPACE_DETAILS_BY_ID = '/i/api/graphql/s2tz6GAie-O1tdZx873PLA/AudioSpaceById',
 }
