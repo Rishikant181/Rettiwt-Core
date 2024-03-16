566372d3e3f05fa342b9fdbf71518ba3a28cf46c
Rishikant Sahu <rishikantsahu181@gmail.com>
2024-03-16 11:24:34 +0530

    Added Post validation groups

---
+++ src/validation/PostGroups.ts
@@ -0,0 +1,9 @@
+// ENUMS
+import { ETweetResources } from '../enums/Resources';
+
+/**
+ * Collection of resources that require a numeric id.
+ *
+ * @internal
+ */
+export const requireNumericId = [ETweetResources.FAVORITE_TWEET, ETweetResources.CREATE_RETWEET];
