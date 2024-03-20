e7ad00507f1e586cf741129ef95fb9f6c73bbd77
Rishikant Sahu <rishikantsahu181@gmail.com>
2024-03-20 16:55:15 +0530

    Linted and formatted code

--- src/index.ts
+++ src/index.ts
@@ -23,10 +23,10 @@ export { Root as IVideoStreamResponse } from './types/media/LiveVideoStream';
 export { Root as ISpaceDetailsResponse } from './types/space/Details';
 
 // TWEETS
-export { Root as ITweetCreateResponse } from './types/tweet/Create';
 export { Root as ITweetDetailsResponse } from './types/tweet/Details';
 export { Root as ITweetLikeResponse } from './types/tweet/Like';
 export { Root as ITweetLikersResponse } from './types/tweet/Likers';
+export { Root as ITweetPostResponse } from './types/tweet/Post';
 export { Root as ITweetRetweetResponse } from './types/tweet/Retweet';
 export { Root as ITweetRetweetersResponse } from './types/tweet/Retweeters';
 export { Root as ITweetSearchResponse } from './types/tweet/Search';
