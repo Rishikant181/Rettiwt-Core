75b547d7d1582484251d80f7cfb7dffb3d378320
Rishikant181 <rishikantsahu181@gmail.com>
2024-09-04 00:45:37 +0530

    Added Notification types

---
+++ src/types/base/Notification.ts
@@ -0,0 +1,65 @@
+/* eslint-disable */
+
+/**
+ * Represents the raw data of a single Notification.
+ *
+ * @public
+ */
+export interface INotification {
+	id: string;
+	timestampMs: string;
+	icon: INotificationIcon;
+	message: INotificationMessage;
+	template: INotificationTemplate;
+}
+
+export interface INotificationIcon {
+	id: string;
+}
+
+export interface INotificationMessage {
+	text: string;
+	entities: INotificationMessageEntity[];
+	rtl: boolean;
+}
+
+export interface INotificationMessageEntity {
+	fromIndex: number;
+	toIndex: number;
+	format: string;
+}
+
+export interface INotificationTemplate {
+	aggregateUserActionsV1: INotificationUserActions;
+}
+
+export interface INotificationUserActions {
+	targetObjects: INotificationTargetObject[];
+	fromUsers: INotificationFromUser[];
+	additionalContext: INotificationAdditionalContext;
+}
+
+export interface INotificationTargetObject {
+	tweet: INotificationTweet;
+}
+
+export interface INotificationTweet {
+	id: string;
+}
+
+export interface INotificationFromUser {
+	user: INotificationUser;
+}
+
+export interface INotificationUser {
+	id: string;
+}
+
+export interface INotificationAdditionalContext {
+	contextText: INotificationContextText;
+}
+
+export interface INotificationContextText {
+	text: string;
+	entities: any[];
+}
