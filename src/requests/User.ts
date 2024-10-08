e4025b85125a5735bfbfab766203518cf0040a56
Ulysses Wu <wdwxy12345@gmail.com>
2024-10-09 01:56:08 +0800

    fetch bookmarks

--- src/requests/User.ts
+++ src/requests/User.ts
@@ -683,3 +683,51 @@ export function unfollow(id: string): AxiosRequestConfig {
 		}),
 	};
 }
+
+/**
+ * @param count - The number of bookmarks to fetch.
+ * @param cursor - The cursor to the batch of bookmarks to fetch.
+ *
+ * @public
+ */
+export function bookmarks(count?: number, cursor?: string): AxiosRequestConfig {
+	return {
+		method: 'get',
+		url: 'https://x.com/i/api/graphql/qToeLeMs43Q8cr7tRYXmaQ/Bookmarks',
+		params: {
+			/* eslint-disable @typescript-eslint/naming-convention */
+			variables: JSON.stringify({
+				count: count,
+				cursor: cursor,
+				includePromotedContent: true,
+			}),
+			features: JSON.stringify({
+				graphql_timeline_v2_bookmark_timeline: true,
+
+				creator_subscriptions_tweet_preview_api_enabled: true,
+				c9s_tweet_anatomy_moderator_badge_enabled: true,
+				tweetypie_unmention_optimization_enabled: true,
+				responsive_web_edit_tweet_api_enabled: true,
+				graphql_is_translatable_rweb_tweet_is_translatable_enabled: true,
+				view_counts_everywhere_api_enabled: true,
+				longform_notetweets_consumption_enabled: true,
+				responsive_web_twitter_article_tweet_consumption_enabled: true,
+				tweet_awards_web_tipping_enabled: false,
+				longform_notetweets_rich_text_read_enabled: true,
+				longform_notetweets_inline_media_enabled: true,
+				rweb_video_timestamps_enabled: true,
+				responsive_web_graphql_exclude_directive_enabled: true,
+				verified_phone_label_enabled: false,
+				freedom_of_speech_not_reach_fetch_enabled: true,
+				standardized_nudges_misinfo: true,
+				tweet_with_visibility_results_prefer_gql_limited_actions_policy_enabled: true,
+				responsive_web_media_download_video_enabled: false,
+				responsive_web_graphql_skip_user_profile_image_extensions_enabled: false,
+				responsive_web_graphql_timeline_navigation_enabled: true,
+				responsive_web_enhance_cards_enabled: false,
+			}),
+			/* eslint-enable @typescript-eslint/naming-convention */
+		},
+		paramsSerializer: { encode: encodeURIComponent },
+	};
+}
